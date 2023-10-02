075cfc13bec203be4847722843340e6d8e4d9ee2
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2023-10-02 10:55:42 +0200

    2/10/2023

--- app/src/main/java/com/example/ex_12/MainActivity.java
+++ app/src/main/java/com/example/ex_12/MainActivity.java
@@ -8,21 +8,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView numero_aleatorio;
+    Random r = new Random();
+    int valorDado = r.nextInt(100)+1;
+    int intentos=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Button button = findViewById(R.id.button);
+        EditText campoTexto = (EditText) findViewById(R.id.mensajeeditTextText);
+        numero_aleatorio= findViewById(R.id.numero);
+        numero_aleatorio.setText(String.valueOf(valorDado));
+        Toast noti= Toast.makeText(this,"G",Toast.LENGTH_SHORT);
+        final Button button = findViewById(R.id.boton);
         button.setOnClickListener(new View.OnClickListener() {
-            final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
-            helloTextView.setText(R.string.user_greeting);
             public void onClick(View v) {
-                EditText campoTexto = (EditText) findViewById(R.id.editTextText);
-                Log.d("Valor ET", campoTexto.getText().toString());
+                int numerointr=Integer.parseInt(campoTexto.getText().toString());
+                if (valorDado>numerointr){
+                    noti.setText("El valor es mayor");
+
+                    noti.show();
+                    intentos+=1;
+                    //Log.i("Valor ET", campoTexto.getText().toString());
+
+                } else if (valorDado<numerointr) {
+                    noti.setText("El valor es menor");
+                    noti.show();
+                    intentos+=1;
+
+                }
+                else {
+                    intentos+=1;
+                    noti.setText("Has acertat"+" Intentos = " +intentos);
+                    noti.show();
+
+                }
 
 
             }
